c0a139b542823be6e604fc0a4f7092dfe56b3821
ocavue <ocavue@gmail.com>
2026-03-14 23:31:17 +1100

    try import

--- e2e/vite8/e2e-vite8.test.ts
+++ e2e/vite8/e2e-vite8.test.ts
@@ -2,7 +2,7 @@ import { runTests } from "../e2e";
 
 runTests(8, async () => ({
   // @ts-ignore: this doesn't work in old node.js version
-  vite: await import("vite"),
+  vite: await import("./node_modules/vite/dist/node/index.js"),
   // @ts-ignore: this doesn't work in old node.js version
   vitePluginLegacy: (await import("@vitejs/plugin-legacy")).default
 }));
