eb1ec5657b460f779780921ddea2e9e6fe688d00
ocavue <ocavue@gmail.com>
2026-03-14 23:34:22 +1100

    update

--- e2e/vite8/e2e-vite8.test.ts
+++ e2e/vite8/e2e-vite8.test.ts
@@ -1,8 +1,8 @@
 import { runTests } from "../e2e";
 
 runTests(8, async () => ({
-  // @ts-ignore: this doesn't work in old node.js version
+  // @ts-ignore: this doesn't work since we're using CommonJS module in tsconfig.json
   vite: await import("./node_modules/vite/dist/node/index.js"),
-  // @ts-ignore: this doesn't work in old node.js version
-  vitePluginLegacy: (await import("@vitejs/plugin-legacy")).default
+  // @ts-ignore: this doesn't work since we're using CommonJS module in tsconfig.json
+  vitePluginLegacy: (await import("./vite8/node_modules/@vitejs/plugin-legacy/dist/index.js")).default
 }));
