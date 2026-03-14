fa2e04ac038ed3fa6a5f1877f12d0f9639c90e19
ocavue <ocavue@gmail.com>
2026-03-15 00:09:34 +1100

    update-checksums

--- e2e/vite8/e2e-vite8.test.ts
+++ e2e/vite8/e2e-vite8.test.ts
@@ -1,10 +1,18 @@
 import { runTests } from "../e2e";
 
-runTests(8, async () => ({
-  // @ts-ignore: this doesn't work since we're using CommonJS module in tsconfig.json
-  vite: await import("vite"),
-  // @ts-ignore: this doesn't work since we're using CommonJS module in tsconfig.json
-  vitePluginLegacy: (await import("@vitejs/plugin-legacy")).default,
+runTests(
+  8,
+  async (): Promise<{
+    vite: typeof import("./node_modules/vite/dist/node/index.js");
+    vitePluginLegacy: (typeof import("./node_modules/@vitejs/plugin-legacy/dist/index.js"))["default"];
+    vitePluginTopLevelAwait?: undefined;
+    vitePluginWasm?: (typeof import("../../src/index.js"))["default"];
+  }> => ({
+    // @ts-ignore: this doesn't work since we're using CommonJS module in tsconfig.json
+    vite: await import("vite"),
+    // @ts-ignore: this doesn't work since we're using CommonJS module in tsconfig.json
+    vitePluginLegacy: (await import("@vitejs/plugin-legacy")).default,
 
-  vitePluginWasm: (await import("vite-plugin-wasm")).default
-}));
+    vitePluginWasm: (await import("vite-plugin-wasm")).default
+  })
+);
